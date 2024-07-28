fbfbca3bd97cd77f74d5a87fb2f3bd062c7ad7de
9weights <68071656+9weights@users.noreply.github.com>
2024-07-28 10:30:59 +0100

    test

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const App = () => {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Testing
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
+export default App
